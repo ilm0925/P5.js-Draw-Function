e974701302819831feea5ea3a4ec817d4b2ad2c9
leejw05 <zeta050525@gmail.com>
2022-08-20 13:40:24 +0900

    css 적용

--- p5.js
+++ p5.js
@@ -2,9 +2,9 @@ let sketch = function (p) {
     windowWidth = window.innerWidth;
     windowHeight = window.innerHeight;
 
-    p.setup = function () {
-        p.createCanvas(windowWidth, windowHeight - 100);
-        p.background("black");
+    p.setup = () => {
+        p.createCanvas(windowWidth, windowHeight);
+        // p.background("black");
         AddValue = 10;
         A = 2;
         X = -(windowWidth / 2) - AddValue;
@@ -20,23 +20,37 @@ let sketch = function (p) {
     }
 
     function coordinate() {
+        p.fill(239, 168, 68);
         p.line(windowWidth / 2, 0, -windowWidth / 2, 0);
         p.line(0, windowHeight / 2, 0, -windowHeight / 2);
         p.stroke(400);
+        p.fill(239, 168, 68);
     }
 
-    p.draw = function draw() {
+    // p.draw = () => {
+    //     p.translate(windowWidth / 2, windowHeight / 2);
+    //     coordinate();
+    //     X += AddValue;
+    //     PY = Y;
+    //     Y = -CosFunc(degreeToRad(X));
+    //     console.log(Y);
+    //     p.line(X - AddValue, PY, X, Y);
+    //     p.stroke(500);
+    //     p.fill("red");
+    //     p.circle(X, -CosFunc(degreeToRad(X)), 10);
+    // };
+
+    p.draw = () => {
         p.translate(windowWidth / 2, windowHeight / 2);
         coordinate();
         X += AddValue;
         PY = Y;
-        Y = -CosFunc(degreeToRad(X));
-        console.log(Y);
+        Y = -(0.03 * X ** 2);
         p.line(X - AddValue, PY, X, Y);
         p.stroke(500);
+        // p.fill(255, 155, 15);
         p.fill("red");
-        p.circle(X, -CosFunc(degreeToRad(X)), 10);
+        p.circle(X, Y, 10);
     };
 };
 new p5(sketch, "container");
-
