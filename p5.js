aade6e49b12a502f054d41ba56914280c5093bc8
leejw05 <zeta050525@gmail.com>
2022-08-21 08:14:07 +0900

    일차함수

--- p5.js
+++ p5.js
@@ -1,56 +1,102 @@
-let sketch = function (p) {
-    windowWidth = window.innerWidth;
-    windowHeight = window.innerHeight;
+speed = null;
+A_value = null;
+B_value = null;
+windowWidth = window.innerWidth;
+windowHeight = window.innerHeight;
 
+function degreeToRad(degree) {
+    return degree * (Math.PI / 180);
+}
+
+function CosFunc(X) {
+    return Math.cos(X) * 200; // 함수(Y값)
+}
+function DrawLine(p, PX, PY, X, Y) {
+    p.line(PX, PY, X, Y);
+    p.stroke(500);
+    p.fill("red");
+    p.circle(X, Y, 10);
+}
+function coordinate(p) {
+    p.fill(239, 168, 68);
+    p.line(windowWidth / 2, 0, -windowWidth / 2, 0);
+    p.line(0, windowHeight / 2, 0, -windowHeight / 2);
+    p.stroke(400);
+    p.fill(239, 168, 68);
+}
+
+function quadraticFunction(p) {
+    p.translate(windowWidth / 2, windowHeight / 2);
+    coordinate(p);
+    X += AddValue;
+    PY = Y;
+    Y = -(A * X ** 2);
+    DrawLine(p, X - AddValue, PY, X, Y);
+}
+
+function LinerFunction(p) {
+    p.translate(windowWidth / 2, windowHeight / 2);
+    coordinate(p);
+    X += AddValue;
+    PY = Y;
+    Y = -(A * X + B_value);
+    DrawLine(p, X - AddValue, PY, X, Y);
+}
+
+let quadratic_Function = function (p) {
     p.setup = () => {
         p.createCanvas(windowWidth, windowHeight);
         // p.background("black");
-        AddValue = 10;
-        A = 2;
+        AddValue = speed;
+        A = A_value;
         X = -(windowWidth / 2) - AddValue;
         Y = 0;
-    };
-
-    function degreeToRad(degree) {
-        return degree * (Math.PI / 180);
-    }
-
-    function CosFunc(X) {
-        return Math.cos(X) * 200; // 함수(Y값)
-    }
-
-    function coordinate() {
-        p.fill(239, 168, 68);
-        p.line(windowWidth / 2, 0, -windowWidth / 2, 0);
-        p.line(0, windowHeight / 2, 0, -windowHeight / 2);
-        p.stroke(400);
-        p.fill(239, 168, 68);
-    }
-
-    // p.draw = () => {
-    //     p.translate(windowWidth / 2, windowHeight / 2);
-    //     coordinate();
-    //     X += AddValue;
-    //     PY = Y;
-    //     Y = -CosFunc(degreeToRad(X));
-    //     console.log(Y);
-    //     p.line(X - AddValue, PY, X, Y);
-    //     p.stroke(500);
-    //     p.fill("red");
-    //     p.circle(X, -CosFunc(degreeToRad(X)), 10);
-    // };
-
-    p.draw = () => {
-        p.translate(windowWidth / 2, windowHeight / 2);
-        coordinate();
-        X += AddValue;
-        PY = Y;
-        Y = -(0.03 * X ** 2);
-        p.line(X - AddValue, PY, X, Y);
-        p.stroke(500);
-        // p.fill(255, 155, 15);
-        p.fill("red");
-        p.circle(X, Y, 10);
+        p.draw = () => {
+            quadraticFunction(p);
+        };
     };
 };
-new p5(sketch, "container");
+
+let Liner_Function = function (p) {
+    p.setup = () => {
+        p.createCanvas(windowWidth, windowHeight);
+        // p.background("black");
+        AddValue = speed;
+        A = A_value;
+        X = -(windowWidth / 2) - AddValue;
+        Y = 0;
+        p.draw = () => {
+            LinerFunction(p);
+        };
+    };
+};
+
+// 이차
+document
+    .querySelector("body > div.btn-box.grid > div:nth-child(2) > button")
+    .addEventListener("click", () => {
+        setTimeout(() => {
+            window.scroll(0, document.body.scrollHeight);
+        }, 1000);
+        speed = Number(document.querySelector("#speed").value);
+        A_value = 0.01 * Number(document.querySelector("#A-value").value);
+        new p5(quadratic_Function, "container");
+    });
+
+// 일차
+document
+    .querySelector("body > div.btn-box.grid > div:nth-child(1) > button")
+    .addEventListener("click", () => {
+        setTimeout(() => {
+            window.scroll(0, document.body.scrollHeight);
+        }, 1000);
+        speed = Number(document.querySelector("#speed").value);
+        A_value = Number(document.querySelector("#A-value").value);
+        B_value = Number(document.querySelector("#B-value").value)*10;
+        new p5(Liner_Function, "container");
+    });
+
+//다시하기
+document.querySelector("body > button").addEventListener("click", () => {
+    location.reload();
+});
