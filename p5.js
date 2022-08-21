83c5b245c71f112915c35eaa624f8ac0e6ce03a9
leejw05 <zeta050525@gmail.com>
2022-08-21 15:13:07 +0900

    querySelector 수정

--- p5.js
+++ p5.js
@@ -141,59 +141,51 @@ let Cos_Function = function (p) {
 };
 
 // 일차
-document
-    .querySelector("body > div.btn-box.grid > div:nth-child(1) > button")
-    .addEventListener("click", () => {
-        setTimeout(() => {
-            window.scroll(0, document.body.scrollHeight);
-        }, 100);
-        speed = Number(document.querySelector("#speed").value);
-        A_value = Number(document.querySelector("#A-value").value);
-        B_value = Number(document.querySelector("#B-value").value) * 10;
-        new p5(Liner_Function, "container");
-    });
+document.querySelectorAll("button")[0].addEventListener("click", () => {
+    setTimeout(() => {
+        window.scroll(0, document.body.scrollHeight);
+    }, 100);
+    speed = Number(document.querySelector("#speed").value);
+    A_value = Number(document.querySelector("#A-value").value);
+    B_value = Number(document.querySelector("#B-value").value) * 10;
+    new p5(Liner_Function, "container");
+});
 // 이차
-document
-    .querySelector("body > div.btn-box.grid > div:nth-child(2) > button")
-    .addEventListener("click", () => {
-        setTimeout(() => {
-            window.scroll(0, document.body.scrollHeight);
-        }, 100);
-        speed = Number(document.querySelector("#speed").value);
-        A_value = 0.01 * Number(document.querySelector("#A-value").value);
-        B_value = Number(document.querySelector("#B-value").value) * 10;
-        C_value = Number(document.querySelector("#C-value").value) * 10;
-        new p5(quadratic_Function, "container");
-    });
+document.querySelectorAll("button")[1].addEventListener("click", () => {
+    setTimeout(() => {
+        window.scroll(0, document.body.scrollHeight);
+    }, 100);
+    speed = Number(document.querySelector("#speed").value);
+    A_value = 0.01 * Number(document.querySelector("#A-value").value);
+    B_value = Number(document.querySelector("#B-value").value) * 10;
+    C_value = Number(document.querySelector("#C-value").value) * 10;
+    new p5(quadratic_Function, "container");
+});
 //Sin함수
-document
-    .querySelector("body > div.btn-box.grid > div:nth-child(3) > button")
-    .addEventListener("click", () => {
-        setTimeout(() => {
-            window.scroll(0, document.body.scrollHeight);
-        }, 100);
-        speed = Number(document.querySelector("#speed").value);
-        A_value = Number(document.querySelector("#A-value").value) * 100;
-        B_value = Number(document.querySelector("#B-value").value) * 10;
-        C_value = Number(document.querySelector("#C-value").value) * 10;
+document.querySelectorAll("button")[2].addEventListener("click", () => {
+    setTimeout(() => {
+        window.scroll(0, document.body.scrollHeight);
+    }, 100);
+    speed = Number(document.querySelector("#speed").value);
+    A_value = Number(document.querySelector("#A-value").value) * 100;
+    B_value = Number(document.querySelector("#B-value").value) * 10;
+    C_value = Number(document.querySelector("#C-value").value) * 10;
 
-        new p5(Sin_Function, "container");
-    });
+    new p5(Sin_Function, "container");
+});
 
 //Cos함수
-document
-    .querySelector("body > div.btn-box.grid > div:nth-child(4) > button")
-    .addEventListener("click", () => {
-        setTimeout(() => {
-            window.scroll(0, document.body.scrollHeight);
-        }, 100);
-        speed = Number(document.querySelector("#speed").value);
-        A_value = Number(document.querySelector("#A-value").value) * 100;
-        B_value = Number(document.querySelector("#B-value").value) * 10;
-        C_value = Number(document.querySelector("#C-value").value) * 10;
+document.querySelectorAll("button")[3].addEventListener("click", () => {
+    setTimeout(() => {
+        window.scroll(0, document.body.scrollHeight);
+    }, 100);
+    speed = Number(document.querySelector("#speed").value);
+    A_value = Number(document.querySelector("#A-value").value) * 100;
+    B_value = Number(document.querySelector("#B-value").value) * 10;
+    C_value = Number(document.querySelector("#C-value").value) * 10;
 
-        new p5(Cos_Function, "container");
-    });
+    new p5(Cos_Function, "container");
+});
 
 //다시하기
 document.querySelector("body > button").addEventListener("click", () => {
