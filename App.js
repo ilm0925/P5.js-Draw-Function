aaff15f2f7ab9a95b261659beeb7139f616054a3
leejw05 <zeta050525@gmail.com>
2022-08-29 00:53:11 +0900

    리팩토링 끝

---
+++ App.js
@@ -0,0 +1,53 @@
+import DrawFuncntion from "./DrawFunction.js";
+
+export default class App {
+    constructor(p){
+        setTimeout(() => {
+            window.scroll(0, document.body.scrollHeight);
+        }, 100);
+    }
+
+    quadratic_Function = function (p) {
+        console.log(p)
+        p.setup = () => {
+            let speed = Number(document.querySelector("#speed").value);
+            let A = 0.01 * Number(document.querySelector("#A-value").value);
+            let B = Number(document.querySelector("#B-value").value) * 10;
+            let C = Number(document.querySelector("#C-value").value) * 10;
+            new DrawFuncntion(p, speed, A, B, C).quadraticFunction();
+        };
+    };
+
+    Liner_Function = function (p) {
+        p.setup = () => {
+            speed = Number(document.querySelector("#speed").value);
+            A = Number(document.querySelector("#A-value").value);
+            B = Number(document.querySelector("#B-value").value) * 10;
+            new DrawFuncntion(p, speed, A, B, 0).LinerFunction();
+        };
+    };
+
+    Sin_Function = function (p) {
+        p.setup = () => {
+            // p.createCanvas(windowWidth, windowHeight);
+            let speed = Number(document.querySelector("#speed").value);
+            let A = Number(document.querySelector("#A-value").value) * 100;
+            let B = Number(document.querySelector("#B-value").value) * 10;
+            let C = Number(document.querySelector("#C-value").value) * 10;
+
+            p.draw = () => new DrawFuncntion(p, speed, A, B, C).SinFunction();
+        };
+    };
+
+    Cos_Function = function (p) {
+        p.setup = () => {
+            // p.createCanvas(windowWidth, windowHeight);
+            let speed = Number(document.querySelector("#speed").value);
+            let A = Number(document.querySelector("#A-value").value) * 100;
+            let B = Number(document.querySelector("#B-value").value) * 10;
+            let C = Number(document.querySelector("#C-value").value) * 10;
+
+            p.draw = () => new DrawFuncntion(p, speed, A, B, C).CosFunction();
+        };
+    };
+}
--- p5.js
+++ p5.js
@@ -1,94 +1,22 @@
-import DrawFuncntion from "./DrawFunction.js";
+import App from "./app.js";
 
-
-let quadratic_Function = function (p) {
-    p.setup = () => {
-        let speed = Number(document.querySelector("#speed").value);
-        let A = 0.01 * Number(document.querySelector("#A-value").value);
-        let B = Number(document.querySelector("#B-value").value) * 10;
-        let C = Number(document.querySelector("#C-value").value) * 10;
-        new DrawFuncntion(p,speed,A,B,C).quadraticFunction()
-    };
-};
-
-let Liner_Function = function (p) {
-    p.setup = () => {
-        p.createCanvas(windowWidth, windowHeight);
-        // p.background("black");
-        AddValue = speed;
-        A = A_value;
-        X = -(windowWidth / 2) - AddValue;
-        Y = 0;
-        RedY = 0;
-        p.draw = () => {
-            LinerFunction(p);
-        };
-    };
-};
-
-let Sin_Function = function (p) {
-    p.setup = () => {
-        p.createCanvas(windowWidth, windowHeight);
-        // p.background("black");
-        AddValue = speed;
-        A = A_value;
-        X = -(windowWidth / 2) - AddValue;
-        Y = 0;
-        RedY = 0;
-        p.draw = () => {
-            SinFunction(p);
-        };
-    };
-};
-
-let Cos_Function = function (p) {
-    p.setup = () => {
-        // p.createCanvas(windowWidth, windowHeight);
-       let speed = Number(document.querySelector("#speed").value);
-       let A = Number(document.querySelector("#A-value").value) * 100;
-       let B = Number(document.querySelector("#B-value").value) * 10;
-       let C = Number(document.querySelector("#C-value").value) * 10;
-
-        p.draw = () => new DrawFuncntion(p,speed,A,B,C).CosFunction()
-    };
-};
 
 // 일차
 document.querySelectorAll("button")[0].addEventListener("click", () => {
-    setTimeout(() => {
-        window.scroll(0, document.body.scrollHeight);
-    }, 100);
-    speed = Number(document.querySelector("#speed").value);
-    A_value = Number(document.querySelector("#A-value").value);
-    B_value = Number(document.querySelector("#B-value").value) * 10;
-    new p5(Liner_Function, "container");
+    new p5(new App().Liner_Function, "container")
 });
 // 이차
 document.querySelectorAll("button")[1].addEventListener("click", () => {
-    setTimeout(() => {
-        window.scroll(0, document.body.scrollHeight);
-    }, 100);
-    new p5(quadratic_Function, "container");
+    new p5(new App().quadratic_Function, "container")
 });
 //Sin함수
 document.querySelectorAll("button")[2].addEventListener("click", () => {
-    setTimeout(() => {
-        window.scroll(0, document.body.scrollHeight);
-    }, 100);
-    speed = Number(document.querySelector("#speed").value);
-    A_value = Number(document.querySelector("#A-value").value) * 100;
-    B_value = Number(document.querySelector("#B-value").value) * 10;
-    C_value = Number(document.querySelector("#C-value").value) * 10;
-
-    new p5(Sin_Function, "container");
+    new p5(new App().Sin_Function, "container")
 });
 
 //Cos함수
 document.querySelectorAll("button")[3].addEventListener("click", () => {
-    setTimeout(() => {
-        window.scroll(0, document.body.scrollHeight);
-    }, 100);
-    new p5(Cos_Function, "container");
+    new p5(new App().Cos_Function, "container")
 });
 
 //다시하기
